17f631953a6b9331600794074363b11f05df01d1
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-06-07 12:56:59 +0200

    mensaje

--- ParaEstudiar/calculator.js
+++ ParaEstudiar/calculator.js
@@ -8,7 +8,7 @@ document
 .querySelector(".screen");
 salida.innerText = numeroInicial;
 }
-else if(event.target.innerText === "7"){
+else if(event.target.innerText === "7" || "6" || "5" || "4" || "3" || "2" || "1" || "8" || "9"){
 let verEnPantalla = 
 document
 .querySelector(".screen");
@@ -22,6 +22,11 @@ if(verEnPantalla.innerText === "0"){
 else{
     verEnPantalla.innerText += event.target.innerText;
 }
+}
+while(event.target.innerText === "+"){
+    let verEnPantalla = 
+    document
+    .querySelector(".screen") + verEnPantalla.innerText;
 }
 });
 //Que funcione la suma y que funcionen todos los numeros
